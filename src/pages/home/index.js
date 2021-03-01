9c5e08e340f4c5d7b9aa93b21ea6473e3dd453ca
Lefcott <leandro.cotti@widergy.com>
2021-03-01 01:34:12 -0300

    webuildwebz app

---
+++ src/pages/home/index.js
@@ -0,0 +1,12 @@
+import { IonPage } from "@ionic/react";
+import "./style.css";
+
+const Home = () => {
+  return (
+    <IonPage className="home">
+      <h1 class="title">WeBuildWebz!</h1>
+    </IonPage>
+  );
+};
+
+export default Home;
